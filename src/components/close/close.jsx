ae58dda55c23cfe023e4df85b4b5498ce06647e8
ViliusZur <vilius.zurauskas@gmail.com>
2020-05-15 18:15:12 +0100

    added a close tab for spotify log in

--- src/components/close/close.jsx
+++ src/components/close/close.jsx
@@ -3,10 +3,32 @@
 import React from "react";
 
 export default function App() {
-    const onClose = () => {
-        window.opener = null;
-        window.open("", "_self");
-        window.close();
+    const onClose = async () => {
+
+      // we use a fetch command to return to back end before closing the window
+      let query = `http://localhost:3300/Spotify/topArtists`;
+    
+      await fetch(query, {
+        method: "GET",
+        headers: 
+        {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        }).then(res => {
+          if(res.ok) {
+              // do something here if the response is ok
+              console.log("response is ok");
+          } else {
+            console.log(query);
+            console.log("error in fetching link");
+          }
+        }
+      );
+
+      window.opener = null;
+      window.open("", "_self");
+      window.close();
     };
   return (
     <>
